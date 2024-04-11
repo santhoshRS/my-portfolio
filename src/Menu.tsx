747cbe5a1adcbc7f8a31e8fc5912c1954a8519c9
santhoshRS <raai.santhosh@gmail.com>
2024-04-11 17:32:44 +0530

    Initial commit

---
+++ src/Menu.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+  
+  const Menu: React.FC = () => {
+    
+    return (
+      <div className="menu">
+        
+      </div>
+    );
+  };
+  
+
+export default Menu;
---
+++ src/Portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Menu';
+
+const Portfolio: React.FC = () => {
+
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <Menu />
+        </div>
+    );
+};
+
+export default Portfolio;
